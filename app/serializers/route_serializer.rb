bc53e5693ecfc52c7cc7bed4eb9d4b1ca8d815e9
skalum <stkalum@gmail.com>
2019-03-11 18:15:36 -0400

    add belongs_to relationship to route serializer

--- app/serializers/route_serializer.rb
+++ app/serializers/route_serializer.rb
@@ -1,3 +1,5 @@
 class RouteSerializer < ActiveModel::Serializer
   attributes :id, :name, :origin, :destination
+  belongs_to :origin, class_name: 'Address', foreign_key: 'origin_id'
+  belongs_to :destination, class_name: 'Address', foreign_key: 'destination_id'
 end
