a5f4f675e9236aefccb3fcd70f8123410778dcbf
skalum <stkalum@gmail.com>
2019-04-16 09:54:43 -0400

    add user to route serializer

--- app/serializers/route_serializer.rb
+++ app/serializers/route_serializer.rb
@@ -2,4 +2,5 @@ class RouteSerializer < ActiveModel::Serializer
   attributes :id, :name, :origin, :destination
   belongs_to :origin, class_name: 'Address', foreign_key: 'origin_id'
   belongs_to :destination, class_name: 'Address', foreign_key: 'destination_id'
+  belongs_to :user
 end
